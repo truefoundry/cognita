e57313292f4675354fa06747e3bb110486ff6d9b
Prathamesh Saraf <prathamesh@mbp.local>
2024-02-13 10:58:12 +0530

    Emptied the __init__.py in retriever

--- backend/modules/retrievers/__init__.py
+++ backend/modules/retrievers/__init__.py
@@ -1,29 +1,32 @@
-from langchain.chat_models.base import BaseChatModel
-from langchain.schema.vectorstore import VectorStore, VectorStoreRetriever
+# TODO: To remove this section later, commenting for now.
 
-from backend.modules.retrievers.default_retriever import DefaultRAGTool
-from backend.utils.base import RetrieverConfig
 
-# TODO: To remove this section later
-def get_retriever(
-    vectorstore: VectorStore,
-    retriever_config: RetrieverConfig,
-    llm: BaseChatModel,
-):
-    """Get retriever."""
-    base_retriever = VectorStoreRetriever(
-        vectorstore=vectorstore,
-        search_type=retriever_config.get_search_type,
-        search_kwargs=retriever_config.get_search_kwargs,
-    )
-    SUPPORTED_RETRIEVERS = [
-        "VectorStoreRetriever",
-        "DefaultRAGTool",
-    ]
-    if retriever_config.class_name not in SUPPORTED_RETRIEVERS:
-        raise ValueError(f"Unsupported retriever: {retriever_config.class_name}")
-    match retriever_config.class_name:
-        case "VectorStoreRetriever":
-            return base_retriever
-        case "DefaultRAGTool":
-            return DefaultRAGTool(retriever=base_retriever, llm=llm)
+# from langchain.chat_models.base import BaseChatModel
+# from langchain.schema.vectorstore import VectorStore, VectorStoreRetriever
+
+# from backend.modules.retrievers.default_retriever import DefaultRAGTool
+# from backend.utils.base import RetrieverConfig
+
+
+# def get_retriever(
+#     vectorstore: VectorStore,
+#     retriever_config: RetrieverConfig,
+#     llm: BaseChatModel,
+# ):
+#     """Get retriever."""
+#     base_retriever = VectorStoreRetriever(
+#         vectorstore=vectorstore,
+#         search_type=retriever_config.get_search_type,
+#         search_kwargs=retriever_config.get_search_kwargs,
+#     )
+#     SUPPORTED_RETRIEVERS = [
+#         "VectorStoreRetriever",
+#         "DefaultRAGTool",
+#     ]
+#     if retriever_config.class_name not in SUPPORTED_RETRIEVERS:
+#         raise ValueError(f"Unsupported retriever: {retriever_config.class_name}")
+#     match retriever_config.class_name:
+#         case "VectorStoreRetriever":
+#             return base_retriever
+#         case "DefaultRAGTool":
+#             return DefaultRAGTool(retriever=base_retriever, llm=llm)
