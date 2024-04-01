f9a2f985e7dc365b3a9d2f11b9227ca856db19bb
Prathamesh Saraf <prathamesh@mbp.local>
2024-04-01 15:41:27 +0530

    Removed tfdocsloader

--- backend/modules/dataloaders/__init__.py
+++ backend/modules/dataloaders/__init__.py
@@ -4,11 +4,9 @@ from backend.modules.dataloaders.loader import register_dataloader
 from backend.modules.dataloaders.localdirloader import LocalDirLoader
 from backend.modules.dataloaders.mlfoundryloader import MlFoundryLoader
 from backend.modules.dataloaders.webloader import WebLoader
-from backend.modules.dataloaders.tfdocsloader import TFDocsArtifactLoader
 
 register_dataloader("local", LocalDirLoader)
 register_dataloader("mlfoundry", MlFoundryLoader)
 register_dataloader("artifact", ArtifactLoader)
 register_dataloader("web", WebLoader)
 register_dataloader("github", GithubLoader)
-register_dataloader("tfdocs-artifact", TFDocsArtifactLoader)
