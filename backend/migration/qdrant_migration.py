6555c6d74df28fd4251a7a02af999c219fa2638f
Prathamesh Saraf <prathamesh@mbp.local>
2024-04-11 19:47:17 +0530

    Added support for cloning qdrant db

---
+++ backend/migration/qdrant_migration.py
@@ -0,0 +1,163 @@
+# This script migrates qdrant collection from one environment to another
+# Environment can be same qdrant instance or different qdrant instance
+"""
+How to run:
+
+python -m backend.migration.qdrant_migration \
+--source_backend_url http://localhost:8000 \
+--source_qdrant_url https://ps-qdrant-prathamesh-ws.devtest.truefoundry.tech \
+--source_collection_name creditcard \
+--destination_backend_url http://localhost:8000 \
+--destination_qdrant_url https://ps-qdrant-prathamesh-ws.devtest.truefoundry.tech \
+--destination_collection_name creditcopy
+
+Add --source_prefix & --destination_prefix if qdrant instance has prefix enabled
+"""
+
+
+import requests, argparse
+
+from backend.types import CreateCollectionDto, AssociateDataSourceWithCollection
+from backend.logger import logger
+
+from qdrant_client import QdrantClient
+from backend.migration.utils import migrate
+
+
+
+
+def migrate_collection(
+    source_backend_url: str,
+    source_collection_name: str,
+    source_qdrant_url: str,
+    source_prefix: str,
+    destination_backend_url: str,
+    destination_collection_name: str,  
+    destination_qdrant_url: str,
+    destination_prefix: str, 
+):
+    # fetch collection from source
+    collections = None
+    with requests.get(f"{source_backend_url.rstrip('/')}/v1/collections/") as r:
+        collections = r.json().get("collections")
+
+    if collections == None:
+        raise Exception("No collections found")
+
+    fetched_collection = None
+    for collection in collections:
+        if collection.get("name") == source_collection_name:
+            fetched_collection = collection
+            break
+
+    if fetched_collection == None:
+        raise Exception(f"Collection {source_collection_name} not found")
+
+    logger.debug(f"Source collection found '{fetched_collection.get('name')}' ")
+
+
+    try:
+        # prepare collection to be created at destination
+        dest_collection = CreateCollectionDto(
+            name=destination_collection_name,
+            description=fetched_collection.get("description", f"Collection cloned from {source_collection_name}"),
+            embedder_config=fetched_collection.get("embedder_config"),
+            associated_data_sources=[
+                AssociateDataSourceWithCollection(
+                    data_source_fqn=value.get("data_source_fqn"),
+                    parser_config=value.get("parser_config"),
+                ) for _, value in fetched_collection.get("associated_data_sources").items()
+            ]
+        ).dict()
+
+        
+        logger.debug(f"Creating '{dest_collection.get('name')}' collection at destination")
+        # create collection at destination
+        with requests.post(
+                url = f"{destination_backend_url.rstrip('/')}/v1/collections/",
+                json = dest_collection
+            ) as r:
+            logger.debug("Collection enrty created: ", r.json())
+
+        logger.debug("Collection migration started...")
+
+        source_qdrant_client = QdrantClient(
+            url=source_qdrant_url,
+            port=443 if source_qdrant_url.startswith("https://") else 6333,
+            prefer_grpc= False if source_qdrant_url.startswith("https://") else True,
+            prefix=source_prefix,
+        )
+
+        destination_qdrant_client = QdrantClient(
+            url=destination_qdrant_url,
+            port=443 if destination_qdrant_url.startswith("https://") else 6333,
+            prefer_grpc= False if destination_qdrant_url.startswith("https://") else True,
+            prefix=destination_prefix,
+        )
+
+        migrate(
+            source_client = source_qdrant_client,
+            dest_client = destination_qdrant_client,
+            source_collection_name = source_collection_name,
+            destination_collection_name = destination_collection_name,
+            batch_size = 100,
+            same_qdrant= (source_qdrant_url == destination_qdrant_url) and  (source_prefix == destination_prefix)
+        )
+
+    except Exception as e:
+        with requests.delete(
+            url = f"{destination_backend_url.rstrip('/')}/v1/collections/{destination_collection_name}",
+            json = dest_collection
+        ) as r:
+            logger.debug("Destination collection entry deleted: ", r.json())    
+        raise e
+        
+    logger.debug(f"Collection '{source_collection_name}' migrated to '{destination_collection_name}' successfully...")
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Migrate qdrant collection from one environment to another')
+
+    # source
+    parser.add_argument('--source_backend_url', type=str, help='Source backend url', required=True)
+    parser.add_argument('--source_collection_name', type=str, help='Source collection name', required=True)
+    parser.add_argument('--source_qdrant_url', type=str, help='Source qdrant url', required=True)
+    parser.add_argument('--source_prefix', type=str, help='Source qdrant prefix', required=False, default=None)
+
+    # destination
+    parser.add_argument('--destination_backend_url', type=str, help='Destination backend url', required=True)
+    parser.add_argument('--destination_collection_name', type=str, help='Destination collection name', required=True)
+    parser.add_argument('--destination_qdrant_url', type=str, help='Destination qdrant url', required=True)
+    parser.add_argument('--destination_prefix', type=str, help='Destination qdrant prefix', required=False, default=None)
+
+    args = parser.parse_args()
+
+    migrate_collection(
+        # source args
+        source_backend_url = args.source_backend_url,
+        source_collection_name = args.source_collection_name,
+        source_qdrant_url = args.source_qdrant_url,
+        source_prefix = args.source_prefix,
+
+        # destination args
+        destination_backend_url = args.destination_backend_url,
+        destination_collection_name = args.destination_collection_name,
+        destination_qdrant_url = args.destination_qdrant_url,
+        destination_prefix = args.destination_prefix
+    )
+
+if __name__ == "__main__":
+    # SOURCE
+    source_backend_url = "http://localhost:8000"
+    source_collection_name = "creditcard"
+    source_qdrant_url = "https://ps-qdrant-prathamesh-ws.devtest.truefoundry.tech"
+    source_prefix = None
+
+    # DESTINATION
+    destination_backend_url = "http://localhost:8000"
+    destination_collection_name = "cloned"
+    destination_qdrant_url = "https://ps-qdrant-prathamesh-ws.devtest.truefoundry.tech"
+    destination_prefix = None
+
+    main()
---
+++ backend/migration/utils.py
@@ -0,0 +1,131 @@
+from typing import Dict
+
+from qdrant_client._pydantic_compat import to_dict
+from qdrant_client.client_base import QdrantBase
+from qdrant_client.http import models
+
+from backend.logger import logger
+
+
+def migrate(
+    source_client: QdrantBase,
+    dest_client: QdrantBase,
+    source_collection_name: str,
+    destination_collection_name: str,
+    batch_size: int = 100,
+    same_qdrant: bool = False,
+) -> None:
+    """
+    Migrate collections from source client to destination client
+
+    Args:
+        source_client (QdrantBase): Source client
+        dest_client (QdrantBase): Destination client
+        source_collection_name (str): source collection names to migrate.
+        destination_collection_name (str): destination collection name.
+        recreate_on_collision (bool, optional): If True - recreate collection if it exists, otherwise
+            raise ValueError.
+        batch_size (int, optional): Batch size for scrolling and uploading vectors. Defaults to 100.
+    """
+    if _has_custom_shards(source_client, source_collection_name):
+        raise ValueError("Migration of collections with custom shards is not supported yet")
+    
+    if source_collection_name == destination_collection_name and same_qdrant:
+        destination_collection_name = destination_collection_name + "copy"
+        logger.debug(f"Destination collection name is same as source collection name. Renaming destination collection to {destination_collection_name}")
+
+    _recreate_collection(
+        source_client=source_client, 
+        dest_client=dest_client, 
+        source_collection_name=source_collection_name, 
+        destination_collection_name=destination_collection_name
+    )
+    _migrate_collection(
+        source_client=source_client, 
+        dest_client=dest_client, 
+        source_collection_name=source_collection_name, 
+        destination_collection_name=destination_collection_name,
+        batch_size=batch_size
+    )
+
+
+def _has_custom_shards(source_client: QdrantBase, collection_name: str) -> bool:
+    collection_info = source_client.get_collection(collection_name)
+    return (
+        getattr(collection_info.config.params, "sharding_method", None)
+        == models.ShardingMethod.CUSTOM
+    )
+
+def _recreate_collection(
+    source_client: QdrantBase,
+    dest_client: QdrantBase,
+    source_collection_name: str,
+    destination_collection_name: str,
+
+) -> None:
+    src_collection_info = source_client.get_collection(source_collection_name)
+    src_config = src_collection_info.config
+    src_payload_schema = src_collection_info.payload_schema
+
+    dest_client.recreate_collection(
+        destination_collection_name,
+        vectors_config=src_config.params.vectors,
+        sparse_vectors_config=src_config.params.sparse_vectors,
+        shard_number=src_config.params.shard_number,
+        replication_factor=src_config.params.replication_factor,
+        write_consistency_factor=src_config.params.write_consistency_factor,
+        on_disk_payload=src_config.params.on_disk_payload,
+        hnsw_config=models.HnswConfigDiff(**to_dict(src_config.hnsw_config)),
+        optimizers_config=models.OptimizersConfigDiff(**to_dict(src_config.optimizer_config)),
+        wal_config=models.WalConfigDiff(**to_dict(src_config.wal_config)),
+        quantization_config=src_config.quantization_config,
+    )
+
+    _recreate_payload_schema(dest_client, destination_collection_name, src_payload_schema)
+
+
+def _recreate_payload_schema(
+    dest_client: QdrantBase,
+    collection_name: str,
+    payload_schema: Dict[str, models.PayloadIndexInfo],
+) -> None:
+    for field_name, field_info in payload_schema.items():
+        dest_client.create_payload_index(
+            collection_name,
+            field_name=field_name,
+            field_schema=field_info.data_type if field_info.params is None else field_info.params,
+        )
+
+
+def _migrate_collection(
+    source_client: QdrantBase,
+    dest_client: QdrantBase,
+    source_collection_name: str,
+    destination_collection_name: str,
+    batch_size: int = 100,
+) -> None:
+    """Migrate collection from source client to destination client
+
+    Args:
+        source_collection_name (str): Collection name of source
+        destination_collection_name (str): Collection name of destination
+        source_client (QdrantBase): Source client
+        dest_client (QdrantBase): Destination client
+        batch_size (int, optional): Batch size for scrolling and uploading vectors. Defaults to 100.
+    """
+    records, next_offset = source_client.scroll(source_collection_name, limit=2, with_vectors=True)
+    dest_client.upload_points(destination_collection_name, records, wait=True)  # type: ignore
+
+    # upload_records has been deprecated due to the usage of models.Record; models.Record has been deprecated as a
+    # structure for uploading due to a `shard_key` field, and now is used only as a result structure.
+    # since shard_keys are not supported in migration, we can safely type ignore here and use Records for uploading
+    while next_offset is not None:
+        records, next_offset = source_client.scroll(
+            source_collection_name, offset=next_offset, limit=batch_size, with_vectors=True
+        )
+        dest_client.upload_points(destination_collection_name, records, wait=True)
+    source_client_vectors_count = source_client.get_collection(source_collection_name).vectors_count
+    dest_client_vectors_count = dest_client.get_collection(destination_collection_name).vectors_count
+    assert (
+        source_client_vectors_count == dest_client_vectors_count
+    ), f"Migration failed, vectors count are not equal: source vector count {source_client_vectors_count}, dest vector count {dest_client_vectors_count}"
