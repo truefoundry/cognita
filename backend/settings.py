a43af01f6b3d6b98e5b18a9eeeecbaa1db14b771
Prathamesh Saraf <prathamesh@mbp.local>
2024-04-26 19:28:45 +0530

    Updated local metadata and added new controller

--- backend/settings.py
+++ backend/settings.py
@@ -62,6 +62,8 @@ class Settings(BaseSettings):
         )
     except Exception as e:
         raise ValueError(f"METADATA_STORE_CONFIG is invalid: {e}")
+    
+    TF_PARSER = os.getenv("TF_PARSER", "")
 
 
 settings = Settings()
