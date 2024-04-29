74cd7f33a13c730423bb05d4950cae3f11838572
Prathamesh Saraf <prathamesh@mbp.local>
2024-04-29 11:50:44 +0530

    Cleanup

--- backend/modules/parsers/__init__.py
+++ backend/modules/parsers/__init__.py
@@ -3,10 +3,8 @@ from backend.modules.parsers.parser import register_parser
 from backend.modules.parsers.pdfparser_fast import PdfParserUsingPyMuPDF
 from backend.modules.parsers.tablepdfparser import PdfTableParser
 from backend.modules.parsers.textparser import TextParser
-from backend.modules.parsers.tfparser import TfParser
 
 register_parser("MarkdownParser", MarkdownParser)
 register_parser("PdfParserFast", PdfParserUsingPyMuPDF)
 register_parser("TextParser", TextParser)
 register_parser("PdfTableParser", PdfTableParser)
-register_parser("TfParser", TfParser)
