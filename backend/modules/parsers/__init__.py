55b449a8e64ee33267488f92b1f44f56cd9acc36
Prathamesh <pratamesh1867@gmail.com>
2024-05-14 15:59:41 +0530

    Code refactoring

--- backend/modules/parsers/__init__.py
+++ backend/modules/parsers/__init__.py
@@ -3,13 +3,17 @@ from backend.modules.parsers.markdownparser import MarkdownParser
 from backend.modules.parsers.multimodal.parser import MultiModalParser
 from backend.modules.parsers.parser import register_parser
 from backend.modules.parsers.pdfparser_fast import PdfParserUsingPyMuPDF
-from backend.modules.parsers.tablepdfparser import PdfTableParser
 from backend.modules.parsers.textparser import TextParser
+from backend.settings import settings
 
 # The order of registry defines the order of precedence
 register_parser("MarkdownParser", MarkdownParser)
 register_parser("TextParser", TextParser)
 register_parser("PdfParserFast", PdfParserUsingPyMuPDF)
-register_parser("PdfTableParser", PdfTableParser)
 register_parser("MultiModalParser", MultiModalParser)
 register_parser("CodeParser", CodeParser)
+
+if settings.LOCAL:
+    from backend.modules.parsers.tablepdfparser import PdfTableParser
+
+    register_parser("PdfTableParser", PdfTableParser)
