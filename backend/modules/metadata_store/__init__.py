88ecb862bcb7525f26fe9342ddc42829c0541399
Prathamesh Saraf <pratamesh1867@gmail.com>
2024-06-20 16:39:33 +0530

    Remove Local MetadataStore and Update ReadMe (#231)
    * Removed local metadata store
    
    * Updated ReadMe
    
    * Update README.md

--- backend/modules/metadata_store/__init__.py
+++ backend/modules/metadata_store/__init__.py
@@ -1,9 +1,7 @@
 from backend.modules.metadata_store.base import register_metadata_store
-from backend.modules.metadata_store.local import LocalMetadataStore
 from backend.modules.metadata_store.truefoundry import TrueFoundry
 from backend.settings import settings
 
-register_metadata_store("local", LocalMetadataStore)
 register_metadata_store("truefoundry", TrueFoundry)
 
 # import of PrismaStore only for Local environment
