24e057748aaa89bed6723dc7ef208010a339bb2a
Prathamesh <pratamesh1867@gmail.com>
2024-06-20 15:29:38 +0530

    Fixed docker-compose for FE

--- frontend/src/screens/dashboard/docsqa/DataSources/index.tsx
+++ frontend/src/screens/dashboard/docsqa/DataSources/index.tsx
@@ -76,20 +76,21 @@ const DataHub = () => {
         </div>
       ),
     },
-    ...(IS_LOCAL_DEVELOPMENT
-      ? [
-          {
-            field: 'actions',
-            headerName: '',
-            width: 80,
-            renderCell: (params: GridRenderCellParams) => (
-              <div className="w-full flex justify-center">
-                <DeleteDataSource fqn={params.row.fqn as string} />
-              </div>
-            ),
-          },
-        ]
-      : []),
+    // TODO: Fix Later
+    // ...(IS_LOCAL_DEVELOPMENT
+    //   ? [
+    //       {
+    //         field: 'actions',
+    //         headerName: '',
+    //         width: 80,
+    //         renderCell: (params: GridRenderCellParams) => (
+    //           <div className="w-full flex justify-center">
+    //             <DeleteDataSource fqn={params.row.fqn as string} />
+    //           </div>
+    //         ),
+    //       },
+    //     ]
+    //   : []),
   ]
   const rows = useMemo(
     () =>
