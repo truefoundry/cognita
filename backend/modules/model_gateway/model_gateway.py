7bef5b3168830e207870c6e32bb510f26961360e
Kwasi Ankomah <kwasi.ankomah@sambanovasystems.com>
2024-06-24 17:01:03 +0100

    SNS intergration v1 commit. Settings.py, model gateway and reqs.txt and controller updates as well as config.json additions.

--- backend/modules/model_gateway/model_gateway.py
+++ backend/modules/model_gateway/model_gateway.py
@@ -1,15 +1,18 @@
 import json
 import os
-from typing import List
+from typing import List, Union
 
 from langchain.embeddings.base import Embeddings
 from langchain_core.language_models.chat_models import BaseChatModel
+from langchain_core.language_models.llms import LLM
 from langchain_openai import OpenAIEmbeddings
 from langchain_openai.chat_models import ChatOpenAI
+from langchain_community.llms.sambanova import SambaStudio
+from langchain_community.llms import Replicate
+from langchain_community.embeddings.sambanova import SambaStudioEmbeddings
 
 from backend.modules.model_gateway import MODELS_CONFIG_PATH
 from backend.types import ModelConfig, ModelProviderConfig, ModelType
-
 
 class ModelGateway:
     provider_configs: List[ModelProviderConfig]
@@ -18,48 +21,24 @@ class ModelGateway:
     def __init__(self):
         with open(MODELS_CONFIG_PATH) as f:
             data = json.load(f)
-            # parse the json data into a list of ModelProviderConfig objects
-            self.provider_configs = [
-                ModelProviderConfig.parse_obj(item) for item in data
-            ]
+            self.provider_configs = [ModelProviderConfig.parse_obj(item) for item in data]
 
-            # load llm models
             self.llm_models: List[ModelConfig] = []
-            # load embedding models
             self.embedding_models: List[ModelConfig] = []
 
             for provider_config in self.provider_configs:
-                if provider_config.api_key_env_var and not os.environ.get(
-                    provider_config.api_key_env_var
-                ):
-                    raise ValueError(
-                        f"Environment variable {provider_config.api_key_env_var} not set. "
-                        f"Cannot initialize the model gateway."
-                    )
+                if provider_config.api_key_env_var and not os.environ.get(provider_config.api_key_env_var):
+                    raise ValueError(f"Environment variable {provider_config.api_key_env_var} not set. Cannot initialize the model gateway.")
 
                 for model_id in provider_config.embedding_model_ids:
                     model_name = f"{provider_config.provider_name}/{model_id}"
                     self.model_name_to_provider_config[model_name] = provider_config
-
-                    # Register the model as an embedding model
-                    self.embedding_models.append(
-                        ModelConfig(
-                            name=f"{provider_config.provider_name}/{model_id}",
-                            type=ModelType.embedding,
-                        )
-                    )
+                    self.embedding_models.append(ModelConfig(name=model_name, type=ModelType.embedding))
 
                 for model_id in provider_config.llm_model_ids:
                     model_name = f"{provider_config.provider_name}/{model_id}"
                     self.model_name_to_provider_config[model_name] = provider_config
-
-                    # Register the model as a llm model
-                    self.llm_models.append(
-                        ModelConfig(
-                            name=f"{provider_config.provider_name}/{model_id}",
-                            type=ModelType.chat,
-                        )
-                    )
+                    self.llm_models.append(ModelConfig(name=model_name, type=ModelType.chat))
 
     def get_embedding_models(self) -> List[ModelConfig]:
         return self.embedding_models
@@ -70,47 +49,54 @@ class ModelGateway:
     def get_embedder_from_model_config(self, model_name: str) -> Embeddings:
         if model_name not in self.model_name_to_provider_config:
             raise ValueError(f"Model {model_name} not registered in the model gateway.")
-        model_provider_config: ModelProviderConfig = self.model_name_to_provider_config[
-            model_name
-        ]
-        if not model_provider_config.api_key_env_var:
-            api_key = "EMPTY"
+        model_provider_config: ModelProviderConfig = self.model_name_to_provider_config[model_name]
+        
+        if model_provider_config.provider_name == "sambastudio":
+            return SambaStudioEmbeddings()
         else:
-            api_key = os.environ.get(model_provider_config.api_key_env_var, "")
-        model_id = "/".join(model_name.split("/")[1:])
-        return OpenAIEmbeddings(
-            openai_api_key=api_key,
-            model=model_id,
-            openai_api_base=model_provider_config.base_url,
-            check_embedding_ctx_length=(
-                model_provider_config.provider_name == "openai"
-            ),
-        )
-
-    def get_llm_from_model_config(
-        self, model_config: ModelConfig, stream=False
-    ) -> BaseChatModel:
-        if model_config.name not in self.model_name_to_provider_config:
-            raise ValueError(
-                f"Model {model_config.name} not registered in the model gateway."
+            api_key = os.environ.get(model_provider_config.api_key_env_var, "") if model_provider_config.api_key_env_var else "EMPTY"
+            model_id = "/".join(model_name.split("/")[1:])
+            return OpenAIEmbeddings(
+                openai_api_key=api_key,
+                model=model_id,
+                openai_api_base=model_provider_config.base_url,
+                check_embedding_ctx_length=(model_provider_config.provider_name == "openai"),
             )
-        model_provider_config: ModelProviderConfig = self.model_name_to_provider_config[
-            model_config.name
-        ]
+
+    def get_llm_from_model_config(self, model_config: ModelConfig, stream=False) -> Union[BaseChatModel, LLM]:
+        if model_config.name not in self.model_name_to_provider_config:
+            raise ValueError(f"Model {model_config.name} not registered in the model gateway.")
+        model_provider_config: ModelProviderConfig = self.model_name_to_provider_config[model_config.name]
+        
         if not model_config.parameters:
             model_config.parameters = {}
-        if not model_provider_config.api_key_env_var:
-            api_key = None
-        else:
-            api_key = os.environ.get(model_provider_config.api_key_env_var, "")
+        
+        api_key = os.environ.get(model_provider_config.api_key_env_var, "") if model_provider_config.api_key_env_var else None
         model_id = "/".join(model_config.name.split("/")[1:])
-        return ChatOpenAI(
-            model=model_id,
-            temperature=model_config.parameters.get("temperature", 0.1),
-            streaming=stream,
-            api_key=api_key,
-            base_url=model_provider_config.base_url,
-        )
 
+        if model_provider_config.provider_name in ["openai", "local-ollama", "local-infinity", "truefoundry"]:
+            return ChatOpenAI(
+                model=model_id,
+                temperature=model_config.parameters.get("temperature", 0.1),
+                streaming=stream,
+                api_key=api_key,
+                base_url=model_provider_config.base_url,
+            )
+        elif model_provider_config.provider_name == "sambastudio":
+            model_kwargs = model_config.parameters.copy() if model_config.parameters else {}
+            model_kwargs["select_expert"] = model_id
+            model_kwargs.update(model_config.parameters)
+            return SambaStudio(
+                streaming=True,
+                model_kwargs=model_kwargs,
+            )
+        elif model_provider_config.provider_name == "replicate":
+            return Replicate(
+                model=model_config.name,
+                streaming=stream,
+                model_kwargs=model_config.parameters
+            )
+        else:
+            raise ValueError(f"Unsupported LLM provider: {model_provider_config.provider_name}")
 
 model_gateway = ModelGateway()
--- backend/settings.py
+++ backend/settings.py
@@ -21,6 +21,22 @@ class Settings(BaseSettings):
     OPENAI_API_KEY: Optional[str]
     TFY_HOST: Optional[str]
     TFY_LLM_GATEWAY_URL: str
+    
+    # Additional Reqs For SambaStudio
+    SAMBASTUDIO_BASE_URL: Optional[str]
+    SAMBASTUDIO_PROJECT_ID: Optional[str]
+    SAMBASTUDIO_ENDPOINT_ID: Optional[str]
+    SAMBASTUDIO_API_KEY: Optional[str]
+    SAMBASTUDIO_EMBEDDINGS_BASE_URL: Optional[str]
+    SAMBASTUDIO_EMBEDDINGS_PROJECT_ID: Optional[str]
+    SAMBASTUDIO_EMBEDDINGS_ENDPOINT_ID: Optional[str]
+    SAMBASTUDIO_EMBEDDINGS_API_KEY: Optional[str]
+
+    REPLICATE_API_TOKEN: Optional[str]
+    WB_PROJECT: Optional[str]
+    WB_STREAM: Optional[str]
+    WB_ENTITY: Optional[str]
+
     LOG_LEVEL = os.getenv("LOG_LEVEL", "info")
     VECTOR_DB_CONFIG = os.getenv("VECTOR_DB_CONFIG", "")
     METADATA_STORE_CONFIG = os.getenv("METADATA_STORE_CONFIG", "")
@@ -36,6 +52,27 @@ class Settings(BaseSettings):
     OLLAMA_URL: str = os.getenv("OLLAMA_URL", "http://localhost:11434")
     EMBEDDING_SVC_URL: str = os.getenv("EMBEDDING_SVC_URL", "")
     RERANKER_SVC_URL: str = os.getenv("RERANKER_SVC_URL", "")
+
+    #SambaStudio Envs
+    SAMBASTUDIO_BASE_URL = os.getenv("SAMBASTUDIO_BASE_URL", "")
+    SAMBASTUDIO_PROJECT_ID = os.getenv("SAMBASTUDIO_PROJECT_ID", "")
+    SAMBASTUDIO_ENDPOINT_ID = os.getenv("SAMBASTUDIO_ENDPOINT_ID", "")
+    SAMBASTUDIO_API_KEY = os.getenv("SAMBASTUDIO_API_KEY", "")
+
+    #SambaStudioEmbeddingSettings
+    SAMBASTUDIO_EMBEDDINGS_BASE_URL = os.getenv("SAMBASTUDIO_EMBEDDINGS_BASE_URL", "")
+    SAMBASTUDIO_EMBEDDINGS_PROJECT_ID = os.getenv("SAMBASTUDIO_EMBEDDINGS_PROJECT_ID", "")
+    SAMBASTUDIO_EMBEDDINGS_ENDPOINT_ID = os.getenv("SAMBASTUDIO_EMBEDDINGS_ENDPOINT_ID", "")
+    SAMBASTUDIO_EMBEDDINGS_API_KEY = os.getenv("SAMBASTUDIO_EMBEDDINGS_API_KEY", "")
+
+    # Replicate LLMs
+    REPLICATE_API_TOKEN = os.getenv("REPLICATE_API_TOKEN", "")
+
+    #WeightsAndBiases
+    WB_PROJECT = "traces"
+    WB_STREAM = "lc_traces_stream"
+    WB_ENTITY= ""
+
 
     if not VECTOR_DB_CONFIG:
         raise ValueError("VECTOR_DB_CONFIG is not set")
@@ -56,6 +93,8 @@ class Settings(BaseSettings):
         )
     except Exception as e:
         raise ValueError(f"METADATA_STORE_CONFIG is invalid: {e}")
+
+
 
 
 settings = Settings()
