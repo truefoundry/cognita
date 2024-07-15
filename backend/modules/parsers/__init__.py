ddb586bed5bce53da166d1e9d40fb8597649d177
Prathamesh <pratamesh1867@gmail.com>
2024-07-15 17:48:09 +0530

    Reordered parser precedence

--- backend/modules/parsers/__init__.py
+++ backend/modules/parsers/__init__.py
@@ -3,5 +3,5 @@ from backend.modules.parsers.parser import register_parser
 from backend.modules.parsers.unstructured_io import UnstructuredIoParser
 
 # The order of registry defines the order of precedence
-register_parser("MultiModalParser", MultiModalParser)
 register_parser("UnstructuredIoParser", UnstructuredIoParser)
+register_parser("MultiModalParser", MultiModalParser)
