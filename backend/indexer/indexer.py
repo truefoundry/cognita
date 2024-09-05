f74aa609ee860a6b63634364babacb6f88a4e476
Kunwar Raj Singh <kunwar31@gmail.com>
2024-09-06 02:45:34 +0530

    run ingestion in background process

--- backend/indexer/indexer.py
+++ backend/indexer/indexer.py
@@ -1,6 +1,8 @@
 import os
+import asyncio
 import tempfile
 from typing import Dict, List
+from multiprocessing import Process
 
 from fastapi import HTTPException
 from fastapi.responses import JSONResponse
@@ -42,17 +44,19 @@ def get_data_point_fqn_to_hash_map(
     return data_point_fqn_to_hash
 
 
-async def sync_data_source_to_collection(inputs: DataIngestionConfig):
+def sync_data_source_to_collection(inputs: DataIngestionConfig):
     """
     Synchronizes the data source to the collection by performing the following steps:
-    1. Updates the data ingestion run status to indicate that existing vectors are being fetched.
-    2. Retrieves the existing data point vectors from the vector store.
-    3. Logs the total number of existing data point vectors in the collection.
-    4. Updates the data ingestion run status to indicate that data ingestion has started.
-    5. Calls the _sync_data_source_to_collection function to perform the actual data ingestion.
-    6. Updates the data ingestion run status to indicate the completion of data ingestion.
-    7. If the data ingestion mode is set to FULL, deletes the outdated data point vectors from the vector store.
-    8. Updates the data ingestion run status to indicate the completion of data cleanup.
+    1. Creates a new event loop for running all async functions
+    2. Updates the data ingestion run status to indicate that existing vectors are being fetched.
+    3. Retrieves the existing data point vectors from the vector store.
+    4. Logs the total number of existing data point vectors in the collection.
+    5. Updates the data ingestion run status to indicate that data ingestion has started.
+    6. Calls the _sync_data_source_to_collection function to perform the actual data ingestion.
+    7. Updates the data ingestion run status to indicate the completion of data ingestion.
+    8. If the data ingestion mode is set to FULL, deletes the outdated data point vectors from the vector store.
+    9. Updates the data ingestion run status to indicate the completion of data cleanup.
+    10. Closes the event loop created in step 1
 
     Args:
         inputs (DataIngestionConfig): The configuration for data ingestion.
@@ -63,11 +67,16 @@ async def sync_data_source_to_collection(inputs: DataIngestionConfig):
     Returns:
         None
     """
-    client = await get_client()
-    await client.aupdate_data_ingestion_run_status(
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    def _run_async(coroutine):
+        return loop.run_until_complete(coroutine)
+    
+    client = _run_async(get_client())
+    _run_async(client.aupdate_data_ingestion_run_status(
         data_ingestion_run_name=inputs.data_ingestion_run_name,
         status=DataIngestionRunStatus.FETCHING_EXISTING_VECTORS,
-    )
+    ))
     try:
         existing_data_point_vectors = VECTOR_STORE_CLIENT.list_data_point_vectors(
             collection_name=inputs.collection_name,
@@ -82,38 +91,38 @@ async def sync_data_source_to_collection(inputs: DataIngestionConfig):
         )
     except Exception as e:
         logger.exception(e)
-        await client.aupdate_data_ingestion_run_status(
+        _run_async( client.aupdate_data_ingestion_run_status(
             data_ingestion_run_name=inputs.data_ingestion_run_name,
             status=DataIngestionRunStatus.FETCHING_EXISTING_VECTORS_FAILED,
-        )
+        ))
         raise e
 
-    await client.aupdate_data_ingestion_run_status(
+    _run_async( client.aupdate_data_ingestion_run_status(
         data_ingestion_run_name=inputs.data_ingestion_run_name,
         status=DataIngestionRunStatus.DATA_INGESTION_STARTED,
-    )
+    ))
     try:
-        await _sync_data_source_to_collection(
+        _run_async( _sync_data_source_to_collection(
             inputs=inputs,
             previous_snapshot=previous_snapshot,
-        )
+        ))
     except Exception as e:
         logger.exception(e)
-        await client.aupdate_data_ingestion_run_status(
+        _run_async( client.aupdate_data_ingestion_run_status(
             data_ingestion_run_name=inputs.data_ingestion_run_name,
             status=DataIngestionRunStatus.DATA_INGESTION_FAILED,
-        )
+        ))
         raise e
-    await client.aupdate_data_ingestion_run_status(
+    _run_async( client.aupdate_data_ingestion_run_status(
         data_ingestion_run_name=inputs.data_ingestion_run_name,
         status=DataIngestionRunStatus.DATA_INGESTION_COMPLETED,
-    )
+    ))
     # Delete the outdated data point vectors from the vector store
     if inputs.data_ingestion_mode == DataIngestionMode.FULL:
-        await client.aupdate_data_ingestion_run_status(
+        _run_async( client.aupdate_data_ingestion_run_status(
             data_ingestion_run_name=inputs.data_ingestion_run_name,
             status=DataIngestionRunStatus.DATA_CLEANUP_STARTED,
-        )
+        ))
         try:
             VECTOR_STORE_CLIENT.delete_data_point_vectors(
                 collection_name=inputs.collection_name,
@@ -121,15 +130,16 @@ async def sync_data_source_to_collection(inputs: DataIngestionConfig):
             )
         except Exception as e:
             logger.exception(e)
-            await client.aupdate_data_ingestion_run_status(
+            _run_async(client.aupdate_data_ingestion_run_status(
                 data_ingestion_run_name=inputs.data_ingestion_run_name,
                 status=DataIngestionRunStatus.DATA_CLEANUP_FAILED,
-            )
+            ))
             raise e
-    await client.aupdate_data_ingestion_run_status(
+    _run_async(client.aupdate_data_ingestion_run_status(
         data_ingestion_run_name=inputs.data_ingestion_run_name,
         status=DataIngestionRunStatus.COMPLETED,
-    )
+    ))
+    loop.close()
 
 
 async def _sync_data_source_to_collection(
@@ -353,8 +363,7 @@ async def ingest_data(request: IngestDataToCollectionDto):
                 created_data_ingestion_run = await client.acreate_data_ingestion_run(
                     data_ingestion_run=data_ingestion_run
                 )
-                await sync_data_source_to_collection(
-                    inputs=DataIngestionConfig(
+                ingestion_config = DataIngestionConfig(
                         collection_name=created_data_ingestion_run.collection_name,
                         data_ingestion_run_name=created_data_ingestion_run.name,
                         data_source=associated_data_source.data_source,
@@ -364,8 +373,8 @@ async def ingest_data(request: IngestDataToCollectionDto):
                         raise_error_on_failure=created_data_ingestion_run.raise_error_on_failure,
                         batch_size=request.batch_size,
                     )
-                )
-                created_data_ingestion_run.status = DataIngestionRunStatus.COMPLETED
+                Process(target=sync_data_source_to_collection, args=(ingestion_config, )).start()
+                created_data_ingestion_run.status = DataIngestionRunStatus.INITIALIZED
             else:
                 if not settings.JOB_FQN:
                     logger.error("Job FQN is required to trigger the job")
